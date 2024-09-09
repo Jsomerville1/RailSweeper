29f88e10a5473f62d5e8eefdefda3f04846fa221
Joey S <100536684+Jsomerville1@users.noreply.github.com>
2024-09-09 03:16:47 -0400

    Fixed mouse look
    May need to change how this is set up in the future. Right now the camera is a child of the platform object in order to keep the camera moving along with the platform. It should work for our purposes but not sure yet.

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -2,35 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseLook : MonoBehaviour
-{
+public class MouseLook : MonoBehaviour {
+    Vector2 rotation = Vector2.zero;
+    public float speed = 3;
 
-
-    public float mouseSensitivity = 100f;
-    public Transform playerBody;  // Reference to the player's body for horizontal rotation
-
-    float xRotation = 0f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Lock the cursor to the center of the screen
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // Get mouse input
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
-        // Adjust the vertical rotation (looking up and down)
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);  // Prevent over-rotation
-
-        // Apply rotations
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);  // Vertical rotation
-        playerBody.Rotate(Vector3.up * mouseX);  // Horizontal rotation
+    void Update () {
+        rotation.y += Input.GetAxis ("Mouse X");
+        rotation.x += -Input.GetAxis ("Mouse Y");
+        transform.eulerAngles = (Vector2)rotation * speed;
     }
 }
